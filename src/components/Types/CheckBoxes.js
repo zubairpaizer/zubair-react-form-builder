c49f22b00362b4e15eabb7a33f4d56321d9ed64c
Zubair Ibrahim <zubair.paizer@hotmail.com>
2018-02-21 12:51:33 +0500

    CheckBoxesInit

---
+++ src/components/Types/CheckBoxes.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CheckBoxes extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <h1>CheckBoxes</h1>
+        )
+    }
+}
+
+export default CheckBoxes;
