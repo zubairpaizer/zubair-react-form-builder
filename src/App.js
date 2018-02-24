2dc31c8c86ba6fb44dd64b6d8f7f62c88d4f31c3
Zubair Ibrahim <zubair.paizer@hotmail.com>
2018-02-24 17:46:44 +0500

    Clean

--- src/App.js
+++ src/App.js
@@ -3,64 +3,6 @@ import ToolBox from './components/ToolBox';
 import FormContainer from './components/FormContainer';
 import './css/App.css';
 
-class Ahsan extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            num1 : 1,
-            num2 : 2
-        }
-    }
-    render(){
-        return <h1>Ahsan</h1>
-    }
-}
-
-
-class CustomComponent extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            num1 : 1,
-            num2 : 2
-        }
-    }
-    render(){
-        return <h1>Custom</h1>
-    }
-}
-
-let customs = [
-    {
-        container : <CustomComponent />,
-        preview : <Ahsan />,
-        toolbox : {
-            title : 'Component',
-            icon : 'fa fa-user',
-            name : 'CUSTOM_COM'
-        },
-        states : {
-            toolType: 'CUSTOM_COM',
-            num1 : 1,
-            num2 : 2
-        }
-    },
-    {
-        container : <Ahsan />,
-        preview : <CustomComponent />,
-        toolbox : {
-            title : 'Ahsan',
-            icon : 'fa fa-user',
-            name : 'ASHAN'
-        },
-        states : {
-            toolType: 'AHSAN',
-            num1 : 1,
-            num2 : 2
-        }
-    }
-]
-
 class App extends Component {
   render() {
     return (
@@ -68,10 +10,10 @@ class App extends Component {
         <div className="container">
             <div className="row">
                 <div className="col-md-8">
-                    <FormContainer custom={customs} />
+                    <FormContainer />
                 </div>
                 <div className="col-md-4">
-                    <ToolBox custom={customs} />
+                    <ToolBox />
                 </div>
             </div>
         </div>
